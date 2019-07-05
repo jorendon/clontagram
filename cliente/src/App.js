de2e7692fdc0be43e8af4c7ac2e3d1be7271a21e
Ricardo Macario <ricardomacario91@gmail.com>
2019-07-05 09:17:13 -0400

    Crear Signup.js e implementar Main.js

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './Componentes/Nav';
 
+import Signup from './Vistas/Signup';
+
 export default function App() {
   return (
-    <div>
+    <div className="ContenedorTemporal">
       <Nav />
-      <h1>¡Bienvenido al curso!</h1>;
+      <Signup />
     </div>
   );
 }
---
+++ cliente/src/Vistas/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from '../Componentes/Main';
+
+export default function Signup() {
+  return (
+    <Main center={true}>
+      <h1>Vista Signup</h1>
+    </Main>
+  );
+}
