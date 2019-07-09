1d1b9645c595f0fc3ccf0294623d17fad295ba4e
Ricardo Macario <ricardomacario91@gmail.com>
2019-07-09 08:16:03 -0400

    Implementar Login.js

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Nav from './Componentes/Nav';
 
 import Signup from './Vistas/Signup';
+import Login from './Vistas/Login';
 
 export default function App() {
   return (
     <div className="ContenedorTemporal">
       <Nav />
-      <Signup />
+      {/* <Signup /> */}
+      <Login />
     </div>
   );
 }
