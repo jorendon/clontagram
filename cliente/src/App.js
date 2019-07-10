92fc3f54d31c340f7c3d585c7be4d9b0aafd5956
Ricardo Macario <ricardomacario91@gmail.com>
2019-07-10 08:01:57 -0400

    Implementar estado usuario en App.js, login, signup y logout

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
+
+import { setToken, deleteToken } from './Helpers/auth-helpers';
 import Nav from './Componentes/Nav';
 
 import Signup from './Vistas/Signup';
 import Login from './Vistas/Login';
 
 export default function App() {
+  const [usuario, setUsuario] = useState(null); // no sabemos si hay un usuario autenticado
+
+  async function login(email, password) {
+    const { data } = await Axios.post('/api/usuarios/login', {
+      email,
+      password
+    });
+    setUsuario(data.usuario);
+    setToken(data.token);
+  }
+
+  async function signup(usuario) {
+    const { data } = await Axios.post('/api/usuarios/signup', usuario);
+    setUsuario(data.usuario);
+    setToken(data.token);
+  }
+
+  function logout() {
+    setUsuario(null);
+    deleteToken();
+  }
+
   return (
     <div className="ContenedorTemporal">
       <Nav />
-      {/* <Signup /> */}
-      <Login />
+      {/* <Signup signup={signup} /> */}
+      <Login login={login} />
+      <div>{JSON.stringify(usuario)}</div>
     </div>
   );
 }
