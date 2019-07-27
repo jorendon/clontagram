24c378f62893c54f89905916674ccab413ad0f44
Ricardo Macario <ricardomacario91@gmail.com>
2019-07-27 10:40:25 -0400

    Implementar Post.js parte 3 - Crear BotonLike.js y Mostar comentarios

--- cliente/src/Componentes/Post.js
+++ cliente/src/Componentes/Post.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Avatar from './Avatar';
+import BotonLike from './BotonLike';
 
 export default function Post({ post, actualizarPost }) {
   const {
@@ -17,6 +19,52 @@ export default function Post({ post, actualizarPost }) {
     <div className="Post-Componente">
       <Avatar usuario={usuario} />
       <img src={url} alt={caption} className="Post-Componente__img" />
+
+      <div className="Post-Componente__acciones">
+        <div className="Post-Componente__like-container">
+          <BotonLike onSubmitLike={() => 1} like={estaLike} />
+        </div>
+        <p>Liked por {numLikes} personas</p>
+        <ul>
+          <li>
+            <Link to={`/perfil/${usuario.username}`}>
+              <b>{usuario.username}</b>
+            </Link>{' '}
+            {caption}
+          </li>
+          <VerTodosLosComentarios _id={_id} numComentarios={numComentarios} />
+          <Comentarios comentarios={comentarios} />
+        </ul>
+      </div>
     </div>
   );
+}
+
+function VerTodosLosComentarios({ _id, numComentarios }) {
+  if (numComentarios < 4) {
+    return null;
+  }
+
+  return (
+    <li className="text-grey-dark">
+      <Link to={`/post/${_id}`}>Ver los {numComentarios} comentarios</Link>
+    </li>
+  );
+}
+
+function Comentarios({ comentarios }) {
+  if (comentarios.length === 0) {
+    return null;
+  }
+
+  return comentarios.map(comentario => {
+    return (
+      <li key={comentario._id}>
+        <Link to={`/perfil/${comentario.usuario.username}`}>
+          <b>{comentario.usuario.username}</b>
+        </Link>{' '}
+        {comentario.mensaje}
+      </li>
+    );
+  });
 }
